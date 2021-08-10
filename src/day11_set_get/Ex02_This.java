87bdd10994ec0fa061874353ac8522d66336744c
Choi.SY <sollly0829@gmail.com>
2021-08-10 10:35:35 +0900

    Ex02_This this 예시

---
+++ src/day11_set_get/Ex02_This.java
@@ -0,0 +1,15 @@
+package day11_set_get;
+
+public class Ex02_This {
+
+	public int age;
+	
+	public void ageFunc(int age) {
+		this.age = age-1;
+	}
+	
+	public void printAge() {
+		System.out.println("당신의 만 나이 : " + age);
+	}
+	
+}
